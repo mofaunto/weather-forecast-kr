acdffd1f77dd2c06708b5a9ffe103799c9da2499
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-01-13 13:05:35 +0900

    Testing API call

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import WeatherInfo from "./components/WeatherInfo";
 import Forecast from "./components/Forecast";
+import getWeatherData from "./api/weatherapi";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = getWeatherData("weather", { q: "Seoul" });
+    console.log(data);
+  };
+
+  fetchWeather();
   return (
     <div className='mx-auto max-w-screen-sm mt-4 py-4 px-16 bg-gradient-to-br from-green-700 to-yellow-700 h-fit shadow-xl shadow-gray-500'>
       <ButtonsHeader />
       <Inputs />
       <TimeAndLocation />
       <WeatherInfo />
-      <Forecast title='Hourly Forecast' />
       <Forecast title='Daily Forecast' />
     </div>
   );
---
+++ src/api/weatherapi.js
@@ -0,0 +1,11 @@
+const API_KEY = "Enter API Key";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  return fetch(url).then((res) => res.json());
+  console.log(url);
+};
+
+export default getWeatherData;
